557750072f0956c42f272c781dda763d0cef8a30
d.bystrov <>
2024-12-19 12:21:03 +0400

    debug

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 TOKEN_TG = "7547250960:AAG6WZ36C8iBozagfggFpEP6QEQwIyyQPpM"
 ACCESS_KEY_UNSPLASH = 'n44z-RVmtjiR-YPAel19A3TUQNnNjBwpElFZ7qeQO1g'
 # ID вашей группы или канала, куда бот будет отправлять сообщения
-CHAT_ID = "-1002334608878"  # Укажите ID вашей группы
+#CHAT_ID = "-1002334608878"  # Укажите ID - test_group
+CHAT_ID = -1002465540172 #Chat ID Мысникова6
 
 PHONE_AVARIA_UK = '759-659'
 PHONE_UPRAV_UK = '+79372255507'
