f5b54638f7d1dd667dff1b3f922387431d3da2d0
d.bystrov <>
2024-12-19 12:16:11 +0400

    debug

--- check_room_id_bot.py
+++ check_room_id_bot.py
@@ -1,22 +1,21 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
 from config import TOKEN_TG
-
 
 # Команда для получения Chat ID
 async def chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.effective_chat.id
-    await update.message.reply_text(f"Chat ID этой группы: {chat_id}")
+    try:
+        chat_id = update.effective_chat.id
+        await update.message.reply_text(f"Chat ID этой группы: {chat_id}")
+    except Exception as e:
+        await update.message.reply_text(f"Произошла ошибка: {e}")
 
-
-
-#if __name__ == "__main__":
+if __name__ == "__main__":
     # Создание приложения
-    #app = ApplicationBuilder().token(TOKEN_TG).build()
+    app = ApplicationBuilder().token(TOKEN_TG).build()
 
     # Добавление обработчика для команды "/chatid"
-    #app.add_handler(CommandHandler("chatid", chat_id))
+    app.add_handler(CommandHandler("chatid", chat_id))
 
     # Запуск бота
-    #app.run_polling(x)
+    app.run_polling()
