2f3ffc3c7b1cbb8ab6915115d31d844ddf9f950f
d.bystrov <>
2025-05-15 12:39:55 +0400

    add phrase

--- handlers.py
+++ handlers.py
@@ -69,7 +69,7 @@ async def reply_to_phrases(update: Update, context: ContextTypes.DEFAULT_TYPE):
             "Показания счетчиков газа можно передать в Приложении или  на сайте 'мой газ'.\n"
             f"{GAS_URL}")
     elif any(word in user_message for word in AD_KEYWORDS):
-        logger.warning("Рекламное сообщение удалено.")
+        logger.warning("SPAM: Рекламное сообщение удалено.")
         await update.message.delete()
 
 
